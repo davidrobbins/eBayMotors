6fdeb3275d82592a0524421a6ec6efe00f7274ca
David Robbins <dave@wakanda.org>
2013-04-15 16:04:17 -0700

    now loading variants.

--- eBayMotors/WebFolder/index.waPage/scripts/index.js
+++ eBayMotors/WebFolder/index.waPage/scripts/index.js
@@ -5,8 +5,55 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
-	function loadModelSelectBox(make) {
-		ds.Model.query("make.title = :1", make, {
+	function loadVehiclesSelectBox(variantId) {
+		/*
+		$('<div>', {
+			text: "test",
+			"class": "vehicleContainer"
+		}).appendTo('#selectVehiclesContainer');
+		*/
+		
+		var divHandle = $('<div>', {
+			text: "one"
+		});
+		
+		var divHandle2 = $('<div>', {
+			text: "two"
+		});
+		
+		var divHandle3 = $('<div>', {
+			
+		});
+		
+		
+		divHandle3.append(divHandle);
+		divHandle3.append(divHandle2);
+		$('#selectVehiclesContainer').append(divHandle3);
+		
+		
+	}
+	
+	function loadVariantSelectBox(modelId) {
+		ds.Variant.query("model.ID == :1", modelId, {
+			onSuccess: function(ev1) {
+				$('#filterVariantContainer').empty();
+				if (ev1.entityCollection.length > 0) {
+					ev1.entityCollection.forEach({
+						onSuccess: function(ev2) {
+							$('<div>', {
+								text: ev2.entity.title.getValue(),
+								"class" : "selectionElement",
+								"data-id" : ev2.entity.ID.getValue()
+							}).appendTo('#filterVariantContainer');
+						}
+					});
+				}
+			}
+		});
+	}
+	
+	function loadModelSelectBox(makeId) {
+		ds.Model.query("make.ID == :1", makeId, {
 			onSuccess: function(ev1) {
 				$('#filterModelContainer').empty();
 				if (ev1.entityCollection.length > 0) {
@@ -14,7 +61,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 						onSuccess: function(ev2) {
 							$('<div>', {
 								text: ev2.entity.title.getValue(),
-								"class" : "make"
+								"class" : "selectionElement",
+								"data-id" : ev2.entity.ID.getValue()
 							}).appendTo('#filterModelContainer');
 						}
 					});
@@ -32,7 +80,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 						//Use jQuery to build the Definition Titles.
 						$('<div>', {
 							text: ev2.entity.title.getValue(),
-							"class" : "make"
+							"class" : "selectionElement",
+							"data-id" : ev2.entity.ID.getValue()
 						}).appendTo('#filterMakeContainer');	
 					}
 				});
@@ -59,8 +108,17 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		$('div.filterContainer').on('click', 'div', function() {
 			$this = $(this);
 			$this.addClass('selected');
+			
 			if ($this.parent().attr("id") == "filterMakeContainer") {
-				loadModelSelectBox($this[0].innerHTML);
+				loadModelSelectBox($this.data("id")); //$this[0].innerHTML
+			}
+			
+			if ($this.parent().attr("id") == "filterModelContainer") {
+				loadVariantSelectBox($this.data("id"));
+			}
+			
+			if ($this.parent().attr("id") == "filterVariantContainer") {
+				loadVehiclesSelectBox($this.data("id"));
 			}
 		});	
 		

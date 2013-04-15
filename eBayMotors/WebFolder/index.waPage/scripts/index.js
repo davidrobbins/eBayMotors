8076f2a6e904325dead0037a299bad75ab65c005
David Robbins <dave@wakanda.org>
2013-04-15 11:52:28 -0700

    now have model list loading on selection of make.

--- eBayMotors/WebFolder/index.waPage/scripts/index.js
+++ eBayMotors/WebFolder/index.waPage/scripts/index.js
@@ -4,21 +4,41 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 // @region namespaceDeclaration// @startlock
 	var documentEvent = {};	// @document
 // @endregion// @endlock
-function loadMakeSelectBox() {
-	ds.Make.all({
-		onSuccess: function(ev1) {
-			ev1.entityCollection.forEach({
-				onSuccess: function(ev2) {	
-					//Use jQuery to build the Definition Titles.
-					$('<div>', {
-						text: ev2.entity.title.getValue(),
-						"class" : "make"
-					}).appendTo('#filterMakeContainer');	
+
+	function loadModelSelectBox(make) {
+		ds.Model.query("make.title = :1", make, {
+			onSuccess: function(ev1) {
+				$('#filterModelContainer').empty();
+				if (ev1.entityCollection.length > 0) {
+					ev1.entityCollection.forEach({
+						onSuccess: function(ev2) {
+							$('<div>', {
+								text: ev2.entity.title.getValue(),
+								"class" : "make"
+							}).appendTo('#filterModelContainer');
+						}
+					});
 				}
-			});
-		} //end - onSuccess: function(ev1)
-	});
-}
+			}
+		});
+	}
+
+
+	function loadMakeSelectBox() {
+		ds.Make.all({
+			onSuccess: function(ev1) {
+				ev1.entityCollection.forEach({
+					onSuccess: function(ev2) {	
+						//Use jQuery to build the Definition Titles.
+						$('<div>', {
+							text: ev2.entity.title.getValue(),
+							"class" : "make"
+						}).appendTo('#filterMakeContainer');	
+					}
+				});
+			} //end - onSuccess: function(ev1)
+		});
+	}
 // eventHandlers// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
@@ -26,17 +46,30 @@ function loadMakeSelectBox() {
 		loadMakeSelectBox();
 		
 			//Create event handler for our Make select.
+			//#filterMakeContainer
+			//div.filterContainer
+		/*
 		$('#filterMakeContainer').on('click', 'div', function() {
 			$this = $(this);
+			loadModelSelectBox($this[0].innerHTML);
+		});	
+		*/
+		
+		
+		$('div.filterContainer').on('click', 'div', function() {
+			$this = $(this);
 			$this.addClass('selected');
+			if ($this.parent().attr("id") == "filterMakeContainer") {
+				loadModelSelectBox($this[0].innerHTML);
+			}
 		});	
 		
-		$('#filterMakeContainer').on('mouseenter', 'div', function() {
+		$('div.filterContainer').on('mouseenter', 'div', function() {
 			$this = $(this);
 			$this.addClass('inFocus');
 		});	
 		
-		$('#filterMakeContainer').on('mouseleave', 'div', function() {
+		$('div.filterContainer').on('mouseleave', 'div', function() {
 			$this.removeClass('inFocus');
 		});	
 		
